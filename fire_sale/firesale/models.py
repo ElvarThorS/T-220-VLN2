f7c4ccb7beb98ef900d9f55da60e5cf7a552179c
Óliver Máni <oliverh21@ru.is>
2022-05-03 13:43:36 +0000

    Setti inn models

--- fire_sale/firesale/models.py
+++ fire_sale/firesale/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+class PersonalInformation(models.Model):
+    name = models.CharField(max_length=255)
+    bio = models.CharField(max_length=8192)
+
+class Image(models.Model):
+    url = models.CharField(max_length=1024)
+
+class User(models.Model):
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=255)
+    personal_information = models.ForeignKey(PersonalInformation, on_delete=models.CASCADE)
+    user_image = models.ForeignKey(Image, on_delete=models.CASCADE)
+
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    seller = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.CharField(max_length=8192)
+    condition = models.CharField(max_length=255)
+
+class ItemImage(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+
+
+
+class Offer(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    price = models.IntegerField()
+    user_offering = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_accepted = models.BooleanField()
+
+class Message(models.Model):
+    to = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.CharField(max_length=8192)
