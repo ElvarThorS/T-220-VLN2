73c1797527b966ef6662cf70b68b2a4207a2d6ed
ElvarThorS <elvar-thor@hotmail.com>
2022-05-03 11:26:25 +0000

    added some more url mappings

--- fire_sale/firesale/urls.py
+++ fire_sale/firesale/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('login/', views.login, name="login"),
+    path('register/', views.register, name="register"),
+    path('dashboard/', views.dashboard, name="dashboard"),
+    path('edit-profile/', views.edit_profile, name="edit_profile"),
 ]
--- fire_sale/firesale/views.py
+++ fire_sale/firesale/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def index(request):
     return HttpResponse('Hello from index view in firesale app')
+
+
+def login(request):
+    return HttpResponse('Here you will login')
+
+
+def register(request):
+    return HttpResponse('Here you will register a new account')
+
+
+def dashboard(request):
+    return HttpResponse('Dashboard will be here')
+
+
+def edit_profile(request):
+    return HttpResponse('Here you will be able to edit your profile')
