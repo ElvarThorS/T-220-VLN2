f8fc4d2cc8c883017e86eae71aa60e8e528e9de7
ElvarThorS <elvar-thor@hotmail.com>
2022-05-06 09:39:38 +0000

    fixes

--- fire_sale/firesale/forms/item_form.py
+++ fire_sale/firesale/forms/item_form.py
@@ -10,14 +10,8 @@ class CreateItemForm(ModelForm):
         model = Item
         exclude = ['id', 'seller']
         widgets = {
-<<<<<<< HEAD
-            'name': widgets.TextInput(attrs={'class': 'form-control'}),
-            #'seller': widgets.TextInput(attrs={'class': 'form-control'}),
-            'description': widgets.TextInput(attrs={'class': 'form-control'}),
-            'condition': widgets.TextInput(attrs={'class': 'form-control'}),
-=======
+
             'name': widgets.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter name'}),
             'condition': widgets.Select(attrs={'class': 'form-control', 'value': 'Select condition'}),
             'description': widgets.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter description'})
->>>>>>> da5e00bd27a9b42cdf6bd608df7941b19192b742
         }
