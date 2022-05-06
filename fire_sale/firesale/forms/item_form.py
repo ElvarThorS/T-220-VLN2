8cddaa501097e11ee40861473afa080d4fa19ade
Óliver Máni <oliverh21@ru.is>
2022-05-06 09:25:06 +0000

    Lagaði nafnið á CreateItemForm

--- fire_sale/firesale/forms/item_form.py
+++ fire_sale/firesale/forms/item_form.py
@@ -4,20 +4,14 @@ from firesale.models import Item
 
 
 class CreateItemForm(ModelForm):
-    image = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter image URL'}))
-
+    image = forms.CharField(required=True, widget=forms.TextInput(attrs={'class':'form-control'}))
+    seller = forms.IntegerField()
     class Meta:
         model = Item
-        exclude = ['id', 'seller']
+        exclude = [ 'id', 'seller' ]
         widgets = {
-<<<<<<< HEAD
             'name': widgets.TextInput(attrs={'class': 'form-control'}),
             #'seller': widgets.TextInput(attrs={'class': 'form-control'}),
             'description': widgets.TextInput(attrs={'class': 'form-control'}),
             'condition': widgets.TextInput(attrs={'class': 'form-control'}),
-=======
-            'name': widgets.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter name'}),
-            'condition': widgets.Select(attrs={'class': 'form-control', 'value': 'Select condition'}),
-            'description': widgets.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter description'})
->>>>>>> da5e00bd27a9b42cdf6bd608df7941b19192b742
         }
