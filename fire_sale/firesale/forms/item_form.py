073da1dadc233e204d027c69eaa952d2b7460755
Óliver Máni <oliverh21@ru.is>
2022-05-06 11:12:13 +0000

    Lagaði item creation form, losaði okkur við seller input, sem á ekki að vera, notandinn á ekki að geta valið hver er að selja hlutinn sinn

--- fire_sale/firesale/forms/item_form.py
+++ fire_sale/firesale/forms/item_form.py
@@ -1,17 +1,24 @@
+from django.contrib.auth.models import User
 from django.forms import ModelForm, widgets
 from django import forms
 from firesale.models import Item
 
 
+
 class CreateItemForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user', '')
+        super(CreateItemForm, self).__init__(*args, **kwargs)
+        self.fields['seller'] = forms.ModelChoiceField(queryset=User.objects,required=False, widget=forms.HiddenInput())
+
     image = forms.CharField(required=True, widget=forms.TextInput(attrs={'class':'form-control'}))
-    seller = forms.IntegerField()
+    #seller = forms.ModelChoiceField(queryset=User.objects.filter(),required=False, widget=forms.HiddenInput())
     class Meta:
         model = Item
-        exclude = [ 'id', 'seller' ]
+        exclude = [ 'id' ]
         widgets = {
             'name': widgets.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter name'}),
             'condition': widgets.Select(attrs={'class': 'form-control', 'value': 'Select condition'}),
-            'description': widgets.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter description'})
+            'description': widgets.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter description'}),
 
         }
--- fire_sale/firesale/views.py
+++ fire_sale/firesale/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from firesale.forms.item_form import CreateItemForm
 from firesale.forms.personal_form import PersonalForm
@@ -36,21 +37,33 @@ def register(request):
 @login_required
 def dashboard(request):
     if request.method == 'POST':
-        form = CreateItemForm(data=request.POST)
+        print("REQUEST USER:", request.user.id)
+        post = request.POST
+        autouser = User()
+        autouser.id = request.user.id
+        autouser.username = request.user.username
+        autouser.first_name = request.user.first_name
+        autouser.last_name = request.user.last_name
+        autouser.email = request.user.email
+        autouser.is_staff = request.user.is_staff
+        autouser.is_active = request.user.is_active
+        autouser.date_joined = request.user.date_joined
+        form = CreateItemForm(data={
+            'name': post['name'],
+            'description': post['description'],
+            'condition': post['condition'],
+            'image': post['image'],
+            'seller': autouser,
+        })
         if form.is_valid():
             form.save()
-            seller = request.user
+
 
     else:
         form = CreateItemForm()
     return render(request, 'firesale/dashboard.html', {
         'form': form
     })
-'''
-@login_required
-def dashboard(request):
-    return render(request, 'firesale/dashboard.html')
-'''
 
 @login_required
 def inbox(request):
