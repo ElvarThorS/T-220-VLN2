9117d6c55dd7f4fd0b5aebea00592a2f14d20445
ElvarThorS <elvar-thor@hotmail.com>
2022-05-12 11:28:57 +0000

    Made checkout prettier

--- fire_sale/firesale/forms/contact_form.py
+++ fire_sale/firesale/forms/contact_form.py
@@ -1,8 +1,8 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    full_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter full name'}), label="Full name", max_length=255)
-    street_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter full name'}), label="Street Name", max_length=255)
-    house_number = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Enter full name'}), label="House Number")
-    country = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter full name'}), label="Country", max_length=255)
-    postal_code = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Enter full name'}), label="Postal Code")
+    full_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your full name'}), label="Full name", max_length=255)
+    street_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your street name'}), label="Street Name", max_length=255)
+    house_number = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Enter your house number'}), label="House Number")
+    country = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your country'}), label="Country", max_length=255)
+    postal_code = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Enter your postal code'}), label="Postal Code")
